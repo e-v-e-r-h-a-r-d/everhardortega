a7f81d07dde15cc6b49bb4574567e7af6e368347
e-v-e-r-h-a-r-d <everhard@cenort.com>
2022-07-03 21:52:46 -0500

    prettier publish

--- components/Footer.js
+++ components/Footer.js
@@ -30,4 +30,3 @@ export default function Footer() {
         </div>
     <div>{siteMetadata.author}</div>
 */
-        
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,12 +10,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  email: 'everhard@cenort.com',
+  github: 'https://github.com/e-v-e-r-h-a-r-d',
+  twitter: 'https://twitter.com/everhard___',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/everhardortega/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the

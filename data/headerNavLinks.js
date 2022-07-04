c3c6f8176b5792032a861f433292467e65272a49
e-v-e-r-h-a-r-d <everhard@cenort.com>
2022-07-03 20:38:40 -0500

    updated sitemetadata

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/ideas', title: 'Ideas' },
+  //{ href: '/tags', title: 'Tags' },
+  { href: '/products', title: 'Products' },
+  { href: '/tweets', title: 'Tweets' },
 ]
 
 export default headerNavLinks
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Everhard Ortega',
+  author: 'Me',
+  headerTitle: 'Everhard Ortega',
+  description: 'This website is for sharing more about me and my ideas',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://everhardortega.com',
+  siteRepo: 'https://github.com/e-v-e-r-h-a-r-d/everhardortega',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',

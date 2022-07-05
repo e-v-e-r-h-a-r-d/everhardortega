3d29770a4ee72af002ab880f188716dae377d0be
e-v-e-r-h-a-r-d <everhard@cenort.com>
2022-07-05 10:13:06 -0500

    all projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,15 +2,15 @@ const projectsData = [
   {
     title: 'Magnesia Healthtech',
     description: `Magnesia is a company established under Mexican law that is 
-    focused in building the next generation of at-home health services`,
+    focused in building the next generation of At-home Health Services`,
     imgSrc: '/static/images/img-magnesi.png',
     href: '/blog/magnesia-healthtech',
   },
   {
     title: 'Cenort',
-    description: `Cenort is the name that I use to offer tech related consulting
-    service to companies with the objective of making good decisions for the future
-    of their business.`,
+    description: `Cenort is the company though which I offer tech related consulting
+    and advicing services to companies with the objective of making good decisions for 
+    the future  of their business.`,
     imgSrc: '/static/images/stack.png',
     href: '/blog/tech-consulting',
   },
@@ -21,6 +21,34 @@ const projectsData = [
     built the Fasel's Web UX.`,
     imgSrc: '/static/images/fasel.png',
     href: '/blog/fasel',
+  },
+  {
+    title: 'Chequeo',
+    description: `Chequeo is a product that helped companies track COVID-19 cases
+    accross their workforce, and avoid propagantions within its facillities.`,
+    imgSrc: '/static/images/chequeo.png',
+    href: '/blog/chequeo',
+  },
+  {
+    title: 'Vincom Analytics',
+    description: `Vincom analytics was a platform that helped companies access key
+    business insights and share them between teams.`,
+    imgSrc: '/static/images/vincom.png',
+    href: '/blog/chequeo',
+  },
+  {
+    title: 'Heartbest Foods',
+    description: `Heartbest is the leading plant-based food brand in Mexico, with
+    presence in more than 500 POS`,
+    imgSrc: '/static/images/Heartbest.png',
+    href: '/blog/chequeo',
+  },
+  {
+    title: 'One Young World Summit',
+    description: `Tu ONE summit is like the olympics for nerds, while I was still 
+    a student at Tec, I won an excellence scholarship to attend and represent Mexico.`,
+    imgSrc: '/static/images/oyw.png',
+    href: '/blog/chequeo',
   },
   {
     title: 'Bonus: One Way Ticket',

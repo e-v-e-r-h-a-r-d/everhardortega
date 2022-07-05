d5107817b19e1bfef2af1a84e5f438bd7fdbf507
e-v-e-r-h-a-r-d <everhard@cenort.com>
2022-07-04 22:19:43 -0500

    new projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -7,12 +7,28 @@ const projectsData = [
     href: '/blog/magnesia-healthtech',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Cenort',
+    description: `Cenort is the name that I use to offer tech related consulting
+    service to companies with the objective of making good decisions for the future
+    of their business.`,
+    imgSrc: '/static/images/stack.png',
+    href: '/blog/tech-consulting',
+  },
+  {
+    title: 'Fasel',
+    description: `Fasel is the easiest way to find the home of your dreams in
+    the city of San Luis Potosí, where I'm originally from. I led the team that
+    built the Fasel's Web UX.`,
+    imgSrc: '/static/images/fasel.png',
+    href: '/blog/fasel',
+  },
+  {
+    title: 'Bonus: One Way Ticket',
+    description: `One Way Ticket is a book and philosophy community by Aniceto 
+    González to whom I helped to publish and distribute on Kindle and Audible. 
+    It has been advertised by Audible as a top pick with other fanous authors.`,
+    imgSrc: '/static/images/owt.png',
+    href: '/blog/one-way-ticket',
   },
 ]
 

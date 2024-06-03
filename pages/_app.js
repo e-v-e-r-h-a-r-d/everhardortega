c736291b53ad6206ec1d28ce2f418399ac68ef8b
e-v-e-r-h-a-r-d <everhard@cenort.com>
2024-06-02 22:42:08 -0700

    back to no analyitcs

--- pages/_app.js
+++ pages/_app.js
@@ -1,80 +1,31 @@
-import '@/css/tailwind.css';
-import '@/css/prism.css';
-import 'katex/dist/katex.css';
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+import 'katex/dist/katex.css'
 
-import '@fontsource/inter/variable-full.css';
+import '@fontsource/inter/variable-full.css'
 
-import { ThemeProvider } from 'next-themes';
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
 
-import siteMetadata from '@/data/siteMetadata';
-import Analytics from '@/components/analytics';
-import LayoutWrapper from '@/components/LayoutWrapper';
-import { ClientReload } from '@/components/ClientReload';
+import siteMetadata from '@/data/siteMetadata'
+import Analytics from '@/components/analytics'
+import LayoutWrapper from '@/components/LayoutWrapper'
+import { ClientReload } from '@/components/ClientReload'
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-const isSocket = process.env.SOCKET;
-
-const GA_TRACKING_ID = 'G-W5DDHK94X6';
-
-// Google Analytics functions
-const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
-
-const event = ({ action, category, label, value }) => {
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
-};
+const isDevelopment = process.env.NODE_ENV === 'development'
+const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
     </ThemeProvider>
-  );
+  )
 }
